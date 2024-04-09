7a25be87d5e589cf68cc342a1139db5dcf9ba62e
Cynthia Valles <cynthiavalles8299@gmail.com>
2024-04-08 22:31:33 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//Section 1: 1. Variables & Data types:
+//A. Variables & Data types:
+
+const myName = "Cynthia";
+let favoriteLanguage = "Python";
+let newFavoriteLanguage = "JavaScript";
+favoriteLanguage = newFavoriteLanguage;
+
+console.log(`My name is ${myName} and my favorite language is ${favoriteLanguage}.`)
+
+
+//------------------------------------------------------------------------------------
+//B. Strings://
+
+let firstVariable = "Hello World!";
+firstVariable = 20;
+let secondVariable = firstVariable;
+console.log(firstVariable);
+let yourName = "Cynthia";
+console.log(`Hello, my name is ${yourName}.`)
+
+
+
+//-------------------------------------------------------------------------------------
+//C.Booleans://
+const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' __ 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true __ false);
+  console.log(false __ false __ false __ false __ false __ true);
+  console.log(false __ false)
+  console.log(e ___ 'Kevin');
+  console.log(a __ b ___ c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a __ a ___ d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 __ '48');
