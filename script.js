1cb23ebd3d99bd092bc8a590ea5f1c0ee6cab106
Cynthia Valles <cynthiavalles8299@gmail.com>
2024-04-08 23:58:41 -0400

    Updated changes.

--- script.js
+++ script.js
@@ -31,12 +31,50 @@ const a = 4;
 
   console.log(a < b);
   console.log(c > d);
-  console.log('Name' __ 'Name');
+  console.log('Name' === 'Name');
   // FOR THE NEXT TWO, USE ONLY && OR ||
-  console.log(true __ false);
-  console.log(false __ false __ false __ false __ false __ true);
-  console.log(false __ false)
-  console.log(e ___ 'Kevin');
-  console.log(a __ b ___ c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
-  console.log(a __ a ___ d); // note: the answer is a simple arithmetic equation, not something "weird"
-  console.log(48 __ '48');
+  console.log(true || false);
+  console.log(false || false || false || false || false || true);
+  console.log(false == false)
+  console.log(e === 'Kevin');
+  console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+  console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
+
+
+  //-----------------------------------------------------------------------------------
+  //D.The Farm://
+  let animal = "cow";
+  if (animal == "cow") {
+    console.log("moooooooo!");
+  } else {
+    console.log("Hey! You are not a cow.");
+  }
+
+  //-----------------------------------------------------------------------------------
+  //E.Driver's Ed//
+  let age= 13;
+  if (age >= 16) {
+    console.log("Here are the keys!");
+  } else { console.log("Sorry, you are too young.");
+  }
+
+  //-----------------------------------------------------------------------------------
+  //Section 3: F.Biggie Smalls://
+  let number = 85;
+  if (number < 100) {
+    console.log("Little number!");
+  } else {
+    console.log("Big number!");
+  }
+  //-----------------------------------------------------------------------------------
+  //G.Monkey in the Middle://
+
+  let number2 = 7;
+  if (number2 < 5) {
+    console.log("little number!");
+  } else if (number2 >10) {
+    console.log("Big number!");
+  } else {
+    console.log("Monkey!")
+  }
